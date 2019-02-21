d169091ae4d9cf5e2c4abb8b96db2a35fc77c720
Neef Rehman <neef.rehman@gmail.com>
2019-02-21 16:22:28 +0000

    reorder for readability

--- lzy.js
+++ lzy.js
@@ -1,31 +1,31 @@
 const lzy = (offset = 200) => {
 
-  const images = document.querySelectorAll("[data-src]");
-
-  const onIntersection = entries => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        observer.unobserve(entry.target);
-        loadImage(entry.target);
-      }
+    const observer = new IntersectionObserver(onIntersection, {
+        rootMargin: `${offset}px ${offset}px`,
+        threshold: 0.01
     });
-  };
 
-  const observer = new IntersectionObserver(onIntersection, {
-    rootMargin: `${offset}px ${offset}px`,
-    threshold: 0.01
-  });
+    const images = document.querySelectorAll("[data-src]");
 
-  const loadImage = imageEl => {
-    const imageSource = imageEl.getAttribute("data-src");
-    if (imageEl.tagName === "IMG") {
-      imageEl.setAttribute("src", imageSource);
-    } else {
-      imageEl.style.backgroundImage = `url(${imageSource})`;
+    const loadImage = imageEl => {
+        const imageSource = imageEl.getAttribute("data-src");
+        if (imageEl.tagName === "IMG") {
+            imageEl.setAttribute("src", imageSource);
+        } else {
+            imageEl.style.backgroundImage = `url(${imageSource})`;
+        }
+        imageEl.removeAttribute("data-src");
+    };
+
+    function onIntersection(entries) {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                observer.unobserve(entry.target);
+                loadImage(entry.target);
+            }
+        });
     }
-    imageEl.removeAttribute("data-src");
-};
 
-  images.forEach(image => observer.observe(image));
+    images.forEach(image => observer.observe(image));
 
 };
